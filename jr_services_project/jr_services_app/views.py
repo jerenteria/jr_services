a975a69ca9e043760d8d1e7a9e22d05e3ed23a94
jerenteria <renteria.juan96@gmail.com>
2022-01-24 22:21:43 -0800

    minor changes

--- jr_services_project/jr_services_app/views.py
+++ jr_services_project/jr_services_app/views.py
@@ -3,57 +3,6 @@ from django.contrib import messages
 import bcrypt
 from .models import *
 ## from flask import Flask, url_for, render_template, request, abort
-from .static import *
-import stripe
-
-app = Flask(__name__)
-
-app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51K2mNoIuO4V9XiaIZxiAWZ8T5yL4bDfygzyKoScqC6zqI6bgsi31XPYOqlCIE2HR0Gx2bCfndIMdm9W0B44r8Cw1009w5WnBex'
-app.config['STRIPE_SECRET_KEY'] = 'sk_test_51K2mNoIuO4V9XiaIb1QuwuZFfL66qeCCpOfmp8F0WzHV72gXpKTrcthFIVAXcXb374ox6OvMlZ2esP0YCJtHlj7c00MjtJnz1w'
-
-stripe.api_key = app.config['STRIPE_SECRET_KEY']
-@app.route('/')
-def stripe():
-    '''
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[{
-            'price': 'price_1K32ZWIuO4V9XiaI1EfUtljP',
-            'quantity': 1,
-        }],
-        mode='payment',
-        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url=url_for('index', _external=True),
-    )
-    '''
-    return render_template(
-        'make_payment.html', 
-        #checkout_session_id=session['id'], 
-        #checkout_public_key=app.config['STRIPE_PUBLIC_KEY']
-    )
-
-@app.route('/stripe_pay')
-def stripe_pay():
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[{
-            'price': 'price_1K32ZWIuO4V9XiaI1EfUtljP',
-            'quantity': 1,
-        }],
-        mode='payment',
-        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url=url_for('make_payment', _external=True),
-    )
-    return {
-        'checkout_session_id': session['id'], 
-        'checkout_public_key': app.config['STRIPE_PUBLIC_KEY']
-    }
-
-
-
-@app.route('/thanks')
-def thanks():
-    return render_template('thanks.html')
 
 def index(request):
     return render(request, "index.html")
@@ -63,39 +12,6 @@ def success(request):
         return redirect('/')
     return render(request, "home.html")
 
-@app.route('/stripe_webhook', methods=['POST'])
-def stripe_webhook():
-    print('WEBHOOK CALLED')
-
-    if request.content_length > 1024 * 1024:
-        print('REQUEST TOO BIG')
-        abort(400)
-    payload = request.get_data()
-    sig_header = request.environ.get('HTTP_STRIPE_SIGNATURE')
-    endpoint_secret = 'whsec_dqKTOFBLyEfLv1KGQKot7ideIkF1vqB9'
-    event = None
-
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, endpoint_secret
-        )
-    except ValueError as e:
-        # Invalid payload
-        print('INVALID PAYLOAD')
-        return {}, 400
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        print('INVALID SIGNATURE')
-        return {}, 400
-
-    # Handle the checkout.session.completed event
-    if event['type'] == 'checkout.session.completed':
-        session = event['data']['object']
-        print(session)
-        line_items = stripe.checkout.Session.list_line_items(session['id'], limit=1)
-        print(line_items['data'][0]['description'])
-
-    return {}
 
 def register(request):
     if request.method=='POST':
